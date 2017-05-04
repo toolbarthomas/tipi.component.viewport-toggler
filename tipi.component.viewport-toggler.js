e49c5fb5ee9b20da2ba16db7409cc737515fbd1e
Thomas van der Velde <contact@toolbarthomas.net>
2017-05-04 14:33:34 +0200

    Updated to ver 2
    Support for Gulp
    Move js in IIFE

--- tipi.component.viewport-toggler.js
+++ tipi.component.viewport-toggler.js
@@ -1,4 +1,4 @@
-function setViewportToggler() {
+(function(win, doc, $) {
     var viewportTogglerElements = {
         'root' : 'viewport-toggler',
         'toggle' : 'viewport-toggler-toggle',
@@ -10,63 +10,65 @@ function setViewportToggler() {
         'active' : '__viewport-toggler--active'
     };
 
-    var viewportToggler = $('.' + viewportTogglerElements.root).not('.' + viewportTogglerStates.ready);
-    if(viewportToggler.length > 0) {
-        viewportToggler.each(function() {
-            var viewportTogglerEach = $(this);
-            var viewportTogglerToggle = getViewportTogglerElement(viewportTogglerEach, 'toggle', viewportTogglerElements);
-            var viewportTogglerItem = getViewportTogglerElement(viewportTogglerEach, 'item', viewportTogglerElements);
+    window.setViewportToggler = function() {
+        var viewportToggler = $('.' + viewportTogglerElements.root).not('.' + viewportTogglerStates.ready);
+        if(viewportToggler.length > 0) {
+            viewportToggler.each(function() {
+                var viewportTogglerEach = $(this);
+                var viewportTogglerToggle = getViewportTogglerElement(viewportTogglerEach, 'toggle', viewportTogglerElements);
+                var viewportTogglerItem = getViewportTogglerElement(viewportTogglerEach, 'item', viewportTogglerElements);
 
-            if(viewportTogglerToggle.length > 0 && viewportTogglerItem.length > 0) {
-                viewportTogglerEach.addClass(viewportTogglerStates.ready);
+                if(viewportTogglerToggle.length > 0 && viewportTogglerItem.length > 0) {
+                    viewportTogglerEach.addClass(viewportTogglerStates.ready);
 
-                viewportTogglerEach.on({
-                    'tipi.viewportToggler.toggle' : function(event, viewportToggler) {
-                        toggleViewportToggler(viewportToggler, viewportTogglerStates);
-                    }
-                });
+                    viewportTogglerEach.on({
+                        'tipi.viewportToggler.toggle' : function(event, viewportToggler) {
+                            toggleViewportToggler(viewportToggler, viewportTogglerStates);
+                        }
+                    });
 
-                viewportTogglerToggle.on({
-                    click : function(event) {
-                        event.preventDefault();
+                    viewportTogglerToggle.on({
+                        click : function(event) {
+                            event.preventDefault();
 
-                        var toggle = $(this);
-                        var viewportToggler = getViewportTogglerElement(toggle, 'root', viewportTogglerElements);
+                            var toggle = $(this);
+                            var viewportToggler = getViewportTogglerElement(toggle, 'root', viewportTogglerElements);
 
-                        if(typeof viewportToggler != 'undefined') {
-                            if(viewportToggler.length > 0) {
-                                viewportToggler.trigger('tipi.viewportToggler.toggle', [viewportToggler]);
+                            if(typeof viewportToggler != 'undefined') {
+                                if(viewportToggler.length > 0) {
+                                    viewportToggler.trigger('tipi.viewportToggler.toggle', [viewportToggler]);
+                                }
                             }
                         }
-                    }
-                })
-            }
-        });
-    }
-}
-
-function toggleViewportToggler(viewportToggler, viewportTogglerStates) {
-    viewportToggler.toggleClass(viewportTogglerStates.active);
-}
-
-function getViewportTogglerElement(origin, type, viewportTogglerElements) {
-    if(typeof origin != 'undefined' && typeof type != 'undefined') {
-        var element;
-
-        switch(type) {
-            case 'root' :
-                element = origin.parents('.' + viewportTogglerElements.root).first();
-                break;
-            case 'toggle' :
-                element = origin.find('.' + viewportTogglerElements.toggle);
-                break;
-            case 'item' :
-                element = origin.find('.' + viewportTogglerElements.item).first();
-                break;
-            default:
-                return;
+                    })
+                }
+            });
         }
-
-        return element;
     }
-}
+
+    function toggleViewportToggler(viewportToggler, viewportTogglerStates) {
+        viewportToggler.toggleClass(viewportTogglerStates.active);
+    }
+
+    function getViewportTogglerElement(origin, type, viewportTogglerElements) {
+        if(typeof origin != 'undefined' && typeof type != 'undefined') {
+            var element;
+
+            switch(type) {
+                case 'root' :
+                    element = origin.parents('.' + viewportTogglerElements.root).first();
+                    break;
+                case 'toggle' :
+                    element = origin.find('.' + viewportTogglerElements.toggle);
+                    break;
+                case 'item' :
+                    element = origin.find('.' + viewportTogglerElements.item).first();
+                    break;
+                default:
+                    return;
+            }
+
+            return element;
+        }
+    }
+})( window.jQuery(window), window.jQuery(document), window.jQuery);
